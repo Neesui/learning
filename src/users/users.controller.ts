25b4e3d4592f14a7f4a1eb0ee59e512732a621cf
Neesui <neeshachy2020@gmail.com>
2025-06-20 19:54:57 +0545

    controller handles CRUD operations (Create, Read, Update, Delete) for users via HTTP routes.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    //GET /users
+   @Get() 
+   findAll(){
+    // GET //users?role=value
+//    findAll(@Query('role') role?: 'ADMIN' | 'USER') 
+    return []
+   }
+
+   //GET /users/:id
+   @Get(':id') 
+   findOne(@Param('id') id: String){
+    return { id }
+   }
+
+   //POST / users
+   @Post()
+   create(@Body() user: {}){
+    return user
+   }
+
+   //PATCH /users/:id
+   @Patch(':id')
+   update(@Param('id') id: string, @Body() userUpdate:{}){
+    return { id, ...userUpdate}
+   }
+
+    //DELETE /users/:id
+   @Delete(':id')
+   delete(@Param('id') id: string){
+    return { id }
+   }
+}
