deb8d44d80dce8a0de3b516bb91e40fdc9611c3e
Neesui <neeshachy2020@gmail.com>
2025-06-21 20:51:52 +0545

    change the code in user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,35 +4,37 @@ import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
 
+        constructor(private readonly userService: UsersService){}
+
     //GET /users
    @Get() 
-   findAll(){
+//    findAll(){
     // GET //users?role=value
-//    findAll(@Query('role') role?: 'ADMIN' | 'USER') 
-    return []
+   findAll(@Query('role') role?: 'ADMIN' | 'USER') {
+    return this.userService.findAll(role)
    }
 
    //GET /users/:id
    @Get(':id') 
    findOne(@Param('id') id: String){
-    return { id }
+    return this.userService.findOne(+id)// add (+) in id is unary plus (convet it into a number)
    }
 
    //POST / users
    @Post()
-   create(@Body() user: {}){
-    return user
+   create(@Body() user: {name: string, email: string, role: 'ADMIN'| 'USER'}){
+    return this.userService.create(user)
    }
 
    //PATCH /users/:id
    @Patch(':id')
-   update(@Param('id') id: string, @Body() userUpdate:{}){
-    return { id, ...userUpdate}
+   update(@Param('id') id: string, @Body() userUpdate:{name?: string, email?: string, role?:'ADMIN' | 'USER'}){
+    return this.userService.update(+id, userUpdate)
    }
 
     //DELETE /users/:id
    @Delete(':id')
    delete(@Param('id') id: string){
-    return { id }
+    return this.userService.delete(+id)
    }
 }
